19c91947879bf2d49b12db6e127de7f9bf9d5830
William Koerich <williamkoerich17@gmail.com>
2026-06-17 21:12:50 -0300

    Envolve o componente RootLayout com AppProvider para gerenciar o estado da aplicação

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,13 @@ import "../global.css";
 
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { AppProvider } from "../store/AppContext";
 
 export default function RootLayout() {
   return (
-    <>
+    <AppProvider>
       <StatusBar style="light" />
       <Stack screenOptions={{ headerShown: false }} />
-    </>
+    </AppProvider>
   );
 }
